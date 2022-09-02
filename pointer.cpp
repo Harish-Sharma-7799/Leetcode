e238a803f3bcf931c6ae090ac02110644e165c8a
Harish Sharma <harishcreate09@gmail.com>
2022-09-02 23:17:15 +0530

    initial commit

---
+++ pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a=10;
+    int *p=&a;
+    int **ptr=&p;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p<<endl;
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    cout<<**ptr<<endl;
+    cout<<&ptr<<endl;
+    cout<<a<<endl;
+    cout<<a<<endl;
+    cout<<a<<endl;
+    cout<<a<<endl;
+    cout<<a<<endl;
+}
+
